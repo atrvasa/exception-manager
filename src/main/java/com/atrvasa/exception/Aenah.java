d04b9defc3086d17c13afafbf6407eea10860b0d
Keykhosrow <atrvasa@gmail.com>
2023-01-14 09:19:58 +0330

    Init project

---
+++ src/main/java/com/atrvasa/exception/Aenah.java
@@ -0,0 +1,18 @@
+/*
+ * MIT License (https://github.com/atrvasa/exception-manager/blob/main/LICENSE)
+ * Repository (https://github.com/atrvasa/exception-manager)
+ */
+
+package com.atrvasa.exception;
+
+/**
+ * Throwable base object for exception.
+ *
+ * <p></p>
+ *
+ * @author  Keykhosrow
+ * 
+ */
+public class Aenah extends Exception {
+
+}
